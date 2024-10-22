e62fd658b739311554f318036eb1935e733c1189
Akshay <akshyadav205261@gmail.com>
2024-10-22 17:30:39 +0530

    remove all tests

--- src/test/java/net/engineeringdigest/journalapp/service/UserServiceTest.java
+++ src/test/java/net/engineeringdigest/journalapp/service/UserServiceTest.java
@@ -1,22 +1,16 @@
 package net.engineeringdigest.journalapp.service;
 
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 public class UserServiceTest {
 
-    @Autowired
+/*    @Autowired
     private UserService userService;
 
     @Disabled
     @ParameterizedTest
-    @ValueSource(ints = {1, 2, 3, 1, 2, 1, 2, 2, 2})
+    @CsvSource({"1,2,3", "2,1,2", "4,1,4"})
     public void test(int a, int b, int c) {
         assertEquals(4, a + b + c);
     }
@@ -26,6 +20,6 @@ public class UserServiceTest {
     @CsvSource({"Akshay", "Madan", "Ram", "Karan"})
     public void testFindByUserName(String name) {
         assertNotNull(userService.findByUserName(name), "Faild for " + name);
-    }
+    }*/
 
 }
