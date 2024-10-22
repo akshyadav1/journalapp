c6d23d704c85b85014c7d0905a1c298dbed7a879
Akshay <akshyadav205261@gmail.com>
2024-10-22 17:25:08 +0530

    disable test

--- src/test/java/net/engineeringdigest/journalapp/service/UserServiceTest.java
+++ src/test/java/net/engineeringdigest/journalapp/service/UserServiceTest.java
@@ -1,5 +1,6 @@
 package net.engineeringdigest.journalapp.service;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -13,13 +14,14 @@ public class UserServiceTest {
     @Autowired
     private UserService userService;
 
-//    @Disabled
+    @Disabled
     @ParameterizedTest
     @ValueSource(ints = {1, 2, 3, 1, 2, 1, 2, 2, 2})
     public void test(int a, int b, int c) {
         assertEquals(4, a + b + c);
     }
 
+    @Disabled
     @ParameterizedTest
     @CsvSource({"Akshay", "Madan", "Ram", "Karan"})
     public void testFindByUserName(String name) {
